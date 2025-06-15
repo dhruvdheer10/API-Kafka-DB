8991e792b1dd4d5497ab20e2fe4b1ea7d3bf87d7
Dhruv Abhijeet Nadkar <nadkar@Dhruvs-MacBook-Air.local>
2025-06-15 04:15:03 -0700

    added indexing on timestamp and symbol

--- app/models/processed_price_point.py
+++ app/models/processed_price_point.py
@@ -8,8 +8,8 @@ class ProcessedPricePoint(Base):
     __tablename__ = "processed_price_points"
 
     id = Column(String, primary_key=True)
-    symbol = Column(String, nullable=False)
+    symbol = Column(String, nullable=False, index=True)
     price = Column(Float, nullable=False)
-    timestamp = Column(DateTime, nullable=False)
+    timestamp = Column(DateTime, nullable=False, index=True)
     provider = Column(String, nullable=False)
     raw_response_id = Column(UUID(as_uuid=(True)), ForeignKey("raw_market_data.id"), nullable=False)
